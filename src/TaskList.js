4db4fdab686a8201b282639d309b9a820fbab01e
ice-009 <21bma009@nith.ac.in>
2023-08-02 20:01:30 +0530

    second edit and delete added

--- src/TaskList.js
+++ src/TaskList.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 const TaskList = ({ newTask }) => {
   const [tasks, setTasks] = useState([]);
+  const [editTaskId, setEditTaskId] = useState(null);
+  const [editTaskData, setEditTaskData] = useState({ heading: '', content: '' });
 
   useEffect(() => {
     async function fetchTasks() {
@@ -16,13 +18,76 @@ const TaskList = ({ newTask }) => {
     fetchTasks();
   }, [newTask]);
 
+  const handleEditClick = (id, heading, content) => {
+    setEditTaskId(id);
+    setEditTaskData({ heading, content });
+  };
+
+  const handleUpdateTask = async (id) => {
+    try {
+      await axios.put(`https://interntaskmanagement.onrender.com/api/tasks/${id}`, editTaskData);
+      setEditTaskId(null);
+      setEditTaskData({ heading: '', content: '' });
+    } catch (error) {
+      console.error('Error updating task:', error);
+    }
+  };
+
+  const handleDeleteTask = async (id) => {
+    try {
+      await axios.delete(`https://interntaskmanagement.onrender.com/api/tasks/${id}`);
+      setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
+    } catch (error) {
+      console.error('Error deleting task:', error);
+    }
+  };
+
   return (
     <div className="mt-4">
       {tasks.map((task) => (
         <div key={task.id} className="card mb-3">
           <div className="card-body">
-            <h3 className="card-title">{task.heading}</h3>
-            <p className="card-text">{task.content}</p>
+            {editTaskId === task.id ? (
+              <>
+                <input
+                  type="text"
+                  value={editTaskData.heading}
+                  onChange={(e) =>
+                    setEditTaskData((prevData) => ({ ...prevData, heading: e.target.value }))
+                  }
+                />
+                <textarea
+                  value={editTaskData.content}
+                  onChange={(e) =>
+                    setEditTaskData((prevData) => ({ ...prevData, content: e.target.value }))
+                  }
+                  rows="3"
+                />
+                <button onClick={() => handleUpdateTask(task.id)} className="btn btn-success mr-2">
+                  Save
+                </button>
+                <button onClick={() => setEditTaskId(null)} className="btn btn-secondary">
+                  Cancel
+                </button>
+              </>
+            ) : (
+              <>
+                <h3 className="card-title">{task.heading}</h3>
+                <p className="card-text">{task.content}</p>
+                <button
+                  onClick={() => handleEditClick(task.id, task.heading, task.content)}
+                  className="btn btn-primary mr-2"
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={() => handleDeleteTask(task.id)}
+                  className="btn btn-danger"
+                >
+                  Delete
+                </button>
+              </>
+            )}
           </div>
         </div>
       ))}
